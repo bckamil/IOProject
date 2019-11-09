88608cdaed96465d27d521e3566804e95dd03bae
KamishiniNoYari <KamishiniNoYari@users.noreply.github.com>
2019-11-09 16:22:08 +0100

    DoubleDecorator Documentation Added

--- src/main/java/pl/put/poznan/transformer/logic/decorators/DoubleDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/DoubleDecorator.java
@@ -5,17 +5,36 @@ import pl.put.poznan.transformer.logic.AbstractInput;
 import java.util.ArrayList;
 
 /**
- * Usuwa powtarzające się wyrazy w najbliższym sąsiedztwie
+ * DoubleDecorator
+ * It takes any object that inherit from AbstractInput class
+ * and modify its previous method getText() to remove multiplications
+ * of adjacent words
+ *
  * @author krystian
+ *
+ * @see pl.put.poznan.transformer.logic.AbstractInput
  */
 
 public class DoubleDecorator extends AbstractDecorator{
 
+    /**
+     * Constructor of DoubleDecorator
+     * It takes any object that inherit from AbstractInput class
+     * and modify its previous method getText()
+     *
+     * @param inputText base object to transform
+     */
     public DoubleDecorator(AbstractInput inputText)
     {
         super(inputText);
     }
 
+    /**
+     * Override getText() method
+     *
+     * @return Returns base text with removed multiplied adjacent words
+     *
+     */
     @Override
     public String getText() {
         String workString = inputText.toString();
