71d597a474403ab5ef177e8932504fc1b8430180
KamishiniNoYari <KamishiniNoYari@users.noreply.github.com>
2019-11-09 16:09:26 +0100

    Double Decorator Added

--- src/main/java/pl/put/poznan/transformer/logic/decorators/DoubleDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/DoubleDecorator.java
@@ -1,4 +1,36 @@
 package pl.put.poznan.transformer.logic.decorators;
 
-public class DoubleDecorator {
+import pl.put.poznan.transformer.logic.AbstractInput;
+
+import java.util.ArrayList;
+
+/**
+ * Usuwa powtarzające się wyrazy w najbliższym sąsiedztwie
+ * @author krystian
+ */
+
+public class DoubleDecorator extends AbstractDecorator{
+
+    public DoubleDecorator(AbstractInput inputText)
+    {
+        super(inputText);
+    }
+
+    @Override
+    public String getText() {
+        String workString = inputText.toString();
+        String[] splittedText = workString.split(" ");
+        int textLen = splittedText.length;
+        ArrayList<String> finalText = new ArrayList<>();
+        finalText.add(splittedText[0]);
+        for(int i=1; i<textLen-1;i++)
+        {
+            if (splittedText[i-1].equals(splittedText[i])){continue;}
+            else{finalText.add(splittedText[i]);}
+
+        }
+        return String.join(" ",finalText);
+    }
+
+
 }
