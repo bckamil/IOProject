942bc1ca70de149b3a6e2f3efcd8ec27ee5878cc
makram89 <34419130+makram89@users.noreply.github.com>
2019-11-09 14:54:16 +0100

    Documentation

--- src/main/java/pl/put/poznan/transformer/logic/decorators/LowerDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/LowerDecorator.java
@@ -3,15 +3,34 @@ package pl.put.poznan.transformer.logic.decorators;
 import pl.put.poznan.transformer.logic.AbstractInput;
 
 /**
- * wszystkie litery na małe: Lower -> lower
+ * LowerDecorator
+ * It takes any object that inherit from AbstractInput class,
+ * and modify its previous method getText() to transform text characters to lower case
+ *
+ * Wszystkie litery na małe: Lower -> lower
+ *
+ * @author marek
+ * @see pl.put.poznan.transformer.logic.decorators.AbstractDecorator
  */
 public class LowerDecorator extends AbstractDecorator {
 
+    /**
+     * Constructor of LowerDecorator
+     * It takes any object that inherit from AbstractInput class,
+     * and modify its previous method getText()
+     *
+     * @param inputText base object to transform
+     */
     public LowerDecorator(AbstractInput inputText) {
         super(inputText);
 
     }
 
+    /**
+     * Override getText() method
+     *
+     * @return Returns base text in lower case
+     */
     @Override
     public String getText()
     {
---
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Package "decorators' contains bunch of decorators classes that transform AbstractInput class.
+ * It also contains abstract-template decorator
+ *
+ * @author krystian
+ * @author marek
+ * @author mariusz
+ * 
+ */
+package pl.put.poznan.transformer.logic.decorators;
