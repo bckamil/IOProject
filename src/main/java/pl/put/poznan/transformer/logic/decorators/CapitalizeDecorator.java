75f24f544d8598a11c3ab5f6e3e3af331792983b
makram89 <34419130+makram89@users.noreply.github.com>
2019-11-11 16:42:33 +0100

    Capitalize decorator repair

--- src/main/java/pl/put/poznan/transformer/logic/decorators/CapitalizeDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/CapitalizeDecorator.java
@@ -4,11 +4,11 @@ import pl.put.poznan.transformer.logic.AbstractInput;
 
 /**
  * CapitalizeDecorator transform text to capitalize firs char of any word in text
-
- *  Pierwsza litera w każdym wyrazie na wielką: capitalize text -> Capitalize Text
+ * <p>
+ * Pierwsza litera w każdym wyrazie na wielką: capitalize text -> Capitalize Text
  *
- *  @author marek
- *  @see pl.put.poznan.transformer.logic.decorators.AbstractDecorator
+ * @author marek
+ * @see pl.put.poznan.transformer.logic.decorators.AbstractDecorator
  */
 public class CapitalizeDecorator extends AbstractDecorator {
 
@@ -16,7 +16,7 @@ public class CapitalizeDecorator extends AbstractDecorator {
      * Constructor of CapitalizeDecorator
      * It takes any object that inherit from AbstractInput class,
      * and modify its previous method getText()
-
+     *
      * @param inputText base object to transform
      */
 
@@ -27,24 +27,35 @@ public class CapitalizeDecorator extends AbstractDecorator {
 
     /**
      * Override getText() method
-     *
+     * <p>
      * ex. capitalize text -> Capitalize Text
-     * @return  Returns base text but every word starts with capital letter
      *
+     * @return Returns base text but every word starts with capital letter
      */
     @Override
     public String getText() {
-        //TODO: Przy inpucie "Oh zwraca "oh bo pierwszym znakiem jest ". Pytanie czy tak może być?
-        String[] tempString = inputText.getText().split(" ");
 
+        String[] tempString = inputText.getText().split(" ");
         for (int j = 0; j < tempString.length; j++)
-            tempString[j] = tempString[j].substring(0, 1).toUpperCase() +
-                    tempString[j].substring(1).toLowerCase();
+
+            for (int i = 0; i < tempString[j].length(); i++) {
+
+                if (Character.isLetter(tempString[j].charAt(i))) {
+                    if (i == 0) {
+                        tempString[j] = tempString[j].substring(0, 1).toUpperCase() +
+                                tempString[j].substring(1).toLowerCase();
+                    } else {
+                        tempString[j] = tempString[j].substring(0, i + 1).toUpperCase() +
+                                tempString[j].substring(i + 1).toLowerCase();
+                    }
+                    break;
+
+                }
+            }
 
         return String.join(" ", tempString);
 
     }
-
 
 
 }
