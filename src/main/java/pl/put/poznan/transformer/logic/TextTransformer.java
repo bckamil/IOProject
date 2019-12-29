8518c897b3365ef49fce9342eea13f3d8ce17c81
marek <marasss.kamin@gmail.com>
2019-12-29 17:45:41 +0100

    FoldDecorator

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -1,10 +1,6 @@
 package pl.put.poznan.transformer.logic;
 
-import pl.put.poznan.transformer.logic.decorators.CapitalizeDecorator;
-import pl.put.poznan.transformer.logic.decorators.DoubleDecorator;
-import pl.put.poznan.transformer.logic.decorators.LowerDecorator;
-import pl.put.poznan.transformer.logic.decorators.UpperDecorator;
-import pl.put.poznan.transformer.logic.decorators.InverseDecorator;
+import pl.put.poznan.transformer.logic.decorators.*;
 
 /**
  * This class manage text transforms
@@ -56,6 +52,11 @@ public class TextTransformer {
         return this.stringInput.getText();
     }
 
+    public String foldTransform(){
+        this.stringInput = new FoldDecorator(stringInput);
+        return this.stringInput.getText();
+    }
+
     public String getInputText() {
         return stringInput.getText();
     }
@@ -87,6 +88,11 @@ public class TextTransformer {
                 case "inverse":
                 {
                     inverseTransform();
+                    break;
+                }
+                case "fold":
+                {
+                    foldTransform();
                     break;
                 }
 
--- src/main/java/pl/put/poznan/transformer/logic/decorators/FoldDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/FoldDecorator.java
@@ -1,22 +1,74 @@
 package pl.put.poznan.transformer.logic.decorators;
 
+
 import pl.put.poznan.transformer.logic.AbstractInput;
+import java.util.ArrayList;
+
+/**
+ * FoldDecorator
+ *
+ * Replace some pre-implemented text values into another -> long veriosns to shortcut.
+ *
+ * @author marek
+ * @see pl.put.poznan.transformer.logic.decorators.AbstractDecorator
+ *
+ *
+ */
+
 
 public class FoldDecorator extends AbstractDecorator {
 
-    static String[] shortcuts = { "np.", "m.in.", "itd."};
-    static String[] sentences = {"na przykład", "między innymi", "i tym podobne"};
 
-//    TODO: 1. copy string, 2. lower copy, 3. find position of sentence 3. in original string replace by index
+    /**
+     * Sentences array stores unfolded version of shortcuts that we want to fold.
+     */
+    static String[] sentences = {"na przykład", "między innymi", "i tym podobne"};
+    /**
+     *  shortcuts array stores folded version of sentences
+     */
+    static String[] shortcuts = { "np.", "m.in.", "itd."};
+
+
+
+    /**
+     * Constructor of LowerDecorator
+     * It takes any object that inherit from AbstractInput class,
+     * and modify its previous method getText()
+     *
+     * @param inputText base object to transform
+     */
     public FoldDecorator(AbstractInput inputText)
     {
         super(inputText);
     }
 
-
+    /**
+     *  Override getText() method
+     *
+     *  Function search predifined words in lowercase ver. of Original  text and replace them in original text.
+     * @return original text with folded phrases
+     */
     @Override
     public String getText() {
 
-        return inputText.getText();
+
+        String tempText = inputText.getText();
+        String textCopy = tempText.toLowerCase();
+
+        for (int counter = 0; counter<3; counter++) {
+            int i = 0;
+            while (true) {
+                int index = textCopy.indexOf(sentences[counter], i);
+                if (index == -1)
+                    break;
+                else {
+                    tempText = tempText.substring(0,index) + shortcuts[counter] + tempText.substring(index+sentences[counter].length());
+                    textCopy = tempText.toLowerCase();
+
+                }
+            }
+        }
+
+        return tempText;
     }
 }
